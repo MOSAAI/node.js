aac96cf69cb1072889e9aa0f55bf533931b19f42
MOSAAI <104287496+MOSAAI@users.noreply.github.com>
2022-05-24 09:12:29 +0700

    24-05-65 Check_Point1
    MOS

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const debug = require('debug')('app')
-const morgan = require('morgan')
+const morgan = require('morgan');
+const path = require('path');
+
 
 const app = express();
-const port = 3000;
+/*const PORT = 4000; */
+const PORT = process.env.PORT || 4000 ;  /* link port page packet.json, second port 4000*/
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public")));
+
 app.get("/", (req,res) => {
-
-    res.send('Hello mosssss');
-
+    res.send('Hello 11111');
+    
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port " + port);
+app.listen(PORT, ()=>{
+    debug("Listening on port " + PORT);
 })
+
+
